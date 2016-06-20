6ec77c760f13c8c4c6b94a7fb735a8dc2979b0b7
celestimon <georgegao96@hotmail.com>
2016-06-19 20:27:58 -0400

    Fully commented Cardset.

--- Cardset.cpp
+++ Cardset.cpp
@@ -14,8 +14,10 @@ namespace {
     }
 }
 
+// Constructor. Initializes an empty set of cards.
 Cardset::Cardset() { }
 
+// Prints the set of cards, displaying only their ranks. Used by GameTable to print cards of each suit separately.
 void Cardset::print(bool rankOnly) const {
     if (rankOnly) {
         string ranks[RANK_COUNT] = { "A", "2", "3", "4", "5", "6",
@@ -31,45 +33,55 @@ void Cardset::print(bool rankOnly) const {
     }
 }
 
-void Cardset::addCard(Card c) {
+// Mutator. Add a specified card to the set.
+void Cardset::addCard(Card& c) {
     if (!contains(c)) {
         cards.push_back(c);
     }
 }
 
-void Cardset::removeCard(Card c) {
+// Mutator. Removes the specified card from the set.
+void Cardset::removeCard(Card& c) {
     cards.erase(remove(cards.begin(), cards.end(), c), cards.end());
 }
 
+// Sorts the set of cards in increasing order by Rank. Used by GameTable to print cards of each suit in proper order.
 void Cardset::sortByRank() {
     sort(cards.begin(), cards.end(), comp);
 }
 
-bool Cardset::contains(Card c) const {
+// Checks if the set contains the specified card.
+bool Cardset::contains(Card& c) const {
     return find(cards.begin(), cards.end(), c) != cards.end();
 }
 
+// Checks if the set contains any cards at all.
 bool Cardset::isEmpty() const {
     return cards.size() == 0;
 }
 
+// Counts the number of cards the set contains.
 int Cardset::size() const {
     cards.begin();
     return cards.size();
 }
 
+// Reset the set to contain no cards.
 void Cardset::clear() {
     cards.clear();
 }
 
+// Accessor. Gets the iterator pointing to the beginning of the set.
 std::vector<Card>::iterator Cardset::begin() {
     return cards.begin();
 }
 
+// Accessor. Gets the iterator pointing to the end of the set.
 std::vector<Card>::iterator Cardset::end() {
     return cards.end();
 }
 
+// Prints out the set of cards in proper text format.
 std::ostream & operator<<(std::ostream& out, const Cardset& cs) {
     for (auto it = cs.cards.begin(); it != cs.cards.end(); it++) {
         out << " " << *it;
--- Cardset.h
+++ Cardset.h
@@ -6,19 +6,90 @@
 
 class Cardset {
 public:
+
     Cardset();
+    // Constructor. Initializes an empty set of cards.
+    // Requires: n/a
+    // Modifies: n/a
+    // Ensures: Created cartset is emtpy
+    // Returns: n/a
+
     void print(bool rankOnly = false) const;
-    void addCard(Card c);
-    void removeCard(Card c);
+    // Prints the set of cards, displaying only their ranks. Used by GameTable to print cards of each suit separately.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: List of cards, if any, is printed. cards[](after) = cards[](before)
+    // Returns: n/a
+
+    void addCard(Card& c);
+    // Mutator. Add a specified card to the set.
+    // Requires: Nothing
+    // Modifies: list of cards
+    // Ensures: cards[](after) = cards[](before) union newly added card
+    // Returns: n/a
+
+    void removeCard(Card& c);
+    // Mutator. Removes the specified card from the set.
+    // Requires: Nothing
+    // Modifies: list of cards
+    // Ensures: cards[](after) = cards[](before) subtract target card, if it exists
+    // Returns: n/a
+
     void sortByRank();
-    bool contains(Card c) const;
+    // Sorts the set of cards in increasing order by Rank. Used by GameTable to print cards of each suit in proper order.
+    // Requires: Nothing
+    // Modifies: order of list of cards
+    // Ensures: For all cards c in cards[](before), c is in cards[](after). No new cards are added or removed
+    // Returns: n/a
+
+    bool contains(Card& c) const;
+    // Checks if the set contains the specified card.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: Card exists in set ? true : false
+
     bool isEmpty() const;
+    // Checks if the set contains any cards at all.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: Is card set empty ? true : false
+
     int size() const;
+    // Counts the number of cards the set contains.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: The number of cards in the set
+
     void clear();
+    // Reset the set to contain no cards.
+    // Requires: Nothing
+    // Modifies: list of cards
+    // Ensures: list of cards is empty after
+    // Returns: n/a
 
     std::vector<Card>::iterator begin();
+    // Accessor. Gets the iterator pointing to the beginning of the set.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: An iterator pointing to the beginning of the set
+
     std::vector<Card>::iterator end();
+    // Accessor. Gets the iterator pointing to the end of the set.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: An iterator pointing to the end of the set
+
     friend std::ostream& operator<< (std::ostream&, const Cardset&);
+    // Prints out the set of cards in proper text format.
+    // Requires: Nothing
+    // Modifies: Nothing
+    // Ensures: cards[](after) = cards[](before)
+    // Returns: An output stream displaying cards in proper text format
     
 private:
     std::vector<Card> cards;
